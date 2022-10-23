c744ef2250fbb73c3b756132bdf9e91ed9d4976a
ms <sergey.muravjev@gmail.com>
2022-10-23 20:03:24 +0300

    chore: tune remark with prettier

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,18 +1,5 @@
-import micromatch from 'micromatch';
-
-// Remark will not honor .remarkignore during lint-stage,
-// so, we do it manually through micromatch.
-// Note, that except .changeset other excluded pathes also
-// excluded in gitignore, so they will never be here.
-
 export default {
+    '*.md': 'pnpm remark:fix',
     '*.{mjs,js,ts,tsx,md,json,yaml}': 'pnpm format:fix',
-    '*.{mjs,js,ts,tsx}': 'pnpm lint:fix',
-    '*.md': files => {
-        const match = micromatch.not(files, '**/.changeset/**');
-        if (match.length === 0) {
-            return [];
-        }
-        return `pnpm remark:fix ${match.join(' ')}`;
-    }
+    '*.{mjs,js,ts,tsx}': 'pnpm lint:fix'
 };
--- packages/remark/index.js
+++ packages/remark/index.js
@@ -1,22 +1,45 @@
 import remarkPresetLintConsistent from 'remark-preset-lint-consistent';
+import remarkPresetLintMarkdownStyleGuide from 'remark-preset-lint-markdown-style-guide';
 import remarkPresetLintRecommended from 'remark-preset-lint-recommended';
+
+import remarkLintNoDuplicateHeadings from 'remark-lint-no-duplicate-headings';
+import remarkLintNoFileNameIrregularCharacters from 'remark-lint-no-file-name-irregular-characters';
+import remarkLintNoUndefinedReferences from 'remark-lint-no-undefined-references';
+
 import remarkLintListItemIndent from 'remark-lint-list-item-indent';
-import remarkPrettier from 'remark-preset-prettier';
+
+import remarkLintFencedCodeFlag from 'remark-lint-fenced-code-flag';
+import remarkValidateLink from 'remark-validate-links';
 import remarkToc from 'remark-toc';
 
+import remarkFrontmatter from 'remark-frontmatter';
+import remarkPrettier from 'remark-preset-prettier';
+
 const remarkConfig = {
-    settings: {
-        bullet: '-', // Use `-` for list item bullets
-        listItemIndent: 'one'
-        // See <https://github.com/remarkjs/remark/tree/main/packages/remark-stringify> for more options.
-    },
     plugins: [
+        //< Add Presets
         remarkPresetLintConsistent, // Check that markdown is consistent.
         remarkPresetLintRecommended, // Few recommended rules.
-        [remarkLintListItemIndent, 'space'],
-        [remarkToc, { heading: 'contents', tight: true }], // Generate a table of contents in `## Contents`
-        remarkPrettier
-    ]
+        remarkPresetLintMarkdownStyleGuide,
+        //< Reconfigure presets
+        [remarkLintNoDuplicateHeadings, false], //< Turn off
+        [remarkLintNoFileNameIrregularCharacters, false], //< Turn off
+        [remarkLintNoUndefinedReferences, false], //< Turn off
+        [remarkLintListItemIndent, 'space'], //< Reconfigure to use space
+        //< Add rules and transformations
+        [remarkLintFencedCodeFlag], // Warn on code block without language flag
+        [remarkValidateLink], //< Warn on invalid links
+        [remarkToc, { heading: 'contents', tight: true }],
+        //< Others
+        remarkFrontmatter, //< Parse frontmatter (---/n ... /n---)
+        remarkPrettier //< Let use prettier for formatting
+    ],
+    settings: {
+        rule: '-',
+        bullet: '-', // Use `-` for list item bullets
+        listItemIndent: 'one',
+        fences: true
+    }
 };
 
 export default remarkConfig;
